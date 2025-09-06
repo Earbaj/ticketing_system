c9fb41f5ddbb8064c513e0e6e43559525976bc6f
Earbaj <earbajsaria3@gmail.com>
2025-09-06 18:23:54 +0600

    ADD:oneboarding screen and add navigation methode to automatically navigate to oneboarding screen after 3 seconds

---
+++ lib/oneboardingScreen/one_boarding.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OneBoarding extends StatefulWidget {
+  const OneBoarding({super.key});
+
+  @override
+  State<OneBoarding> createState() => _OneBoardingState();
+}
+
+class _OneBoardingState extends State<OneBoarding> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
