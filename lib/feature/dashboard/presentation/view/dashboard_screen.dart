da9f17bdc4becf00d4756223f996fbd04104274b
Earbaj <earbajsaria3@gmail.com>
2025-12-27 15:46:12 +0600

    ADD:ticket details Screen and add ticket model

--- lib/feature/dashboard/presentation/view/dashboard_screen.dart
+++ lib/feature/dashboard/presentation/view/dashboard_screen.dart
@@ -1,29 +1,30 @@
-
 import 'package:flutter/material.dart';
 
 import '../../../auth/presentation/view/login_screen.dart';
+import '../../../details/data/model/ticket_details.dart';
+import '../../../details/presentation/view/ticket_details_screen.dart';
 import '../widget/ticket_card.dart';
 
-class DashboardScreen extends StatefulWidget  {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
 
   @override
   State<DashboardScreen> createState() => _DashboardScreenState();
 }
 
-class _DashboardScreenState extends State<DashboardScreen>  with SingleTickerProviderStateMixin {
-
+class _DashboardScreenState extends State<DashboardScreen>
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
   final List<String> busNames = [
     'ALL Travels',
-    'SR Travels',
-    'Shymoli NR Travels',
-    'Shymoli SR Travels',
-    'Hanif Travels',
-    'Orin Travels',
-    'Green Line Travels',
-    'Alhamra Travels',
+    'BS Travels',
+    'SS NR Travels',
+    'TS SR Travels',
+    'GF Travels',
+    'FD Travels',
+    'DD Travels',
+    'AA Travels',
   ];
 
   @override
@@ -47,69 +48,105 @@ class _DashboardScreenState extends State<DashboardScreen>  with SingleTickerPro
       backgroundColor: Colors.white,
       body: SafeArea(
           child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.only(left: 15,right: 15),
-                height: 70,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Color(0xFFffe696e)
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            padding: EdgeInsets.only(left: 15, right: 15),
+            height: 70,
+            width: double.infinity,
+            decoration: BoxDecoration(color: Color(0xFFffe696e)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
                   children: [
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 25, // Sets the size of the avatar
-                          backgroundColor: Colors.white, // Color displayed if no image is present
-                          child: Text(
-                            'ES',
-                            style: TextStyle(fontSize: 20.0, color: Colors.black45),
-                          ), // Child widget, e.g., user initials
-                        ),
-                        SizedBox(width: 10,),
-                        Text(
-                          'Earbaj Md Saria',
-                          style: TextStyle(fontSize: 20.0, color: Colors.white,fontWeight: FontWeight.bold),
-                        ),
-                      ],
+                    CircleAvatar(
+                      radius: 25,
+                      // Sets the size of the avatar
+                      backgroundColor: Colors.white,
+                      // Color displayed if no image is present
+                      child: Text(
+                        'ES',
+                        style: TextStyle(fontSize: 20.0, color: Colors.black45),
+                      ), // Child widget, e.g., user initials
                     ),
-                    GestureDetector(
-                        onTap: (){
-                          Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginScreen()));
-                        },
-                        child: Icon(Icons.logout,color: Colors.white,)),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'Earbaj Md Saria',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ],
                 ),
-              ),
-              DefaultTabController(
-                length: 8, // Number of tabs
-                child: TabBar(
-                  controller: _tabController,
-                  isScrollable: true, // This enables horizontal scrolling
-                  tabAlignment: TabAlignment.start, // Aligns tabs to the left
-                  indicatorColor: Colors.black26,
-                  labelColor: Colors.black,
-                  unselectedLabelColor: Colors.black26,
-                  tabs: busNames.map((name) => Tab(text: name)).toList(),
-                )
-              ),
-              Expanded(
-                child: ListView.builder(
-                  padding: EdgeInsets.all(16),
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: EdgeInsets.only(bottom: 12),
-                      child: TicketCard(busName:  busNames[_tabController.index],),
-                    );
-                  },
-                ),
-              ),
-            ],
-          )
-      ),
+                GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LoginScreen()));
+                    },
+                    child: Icon(
+                      Icons.logout,
+                      color: Colors.white,
+                    )),
+              ],
+            ),
+          ),
+          DefaultTabController(
+              length: 8, // Number of tabs
+              child: TabBar(
+                controller: _tabController,
+                isScrollable: true,
+                // This enables horizontal scrolling
+                tabAlignment: TabAlignment.start,
+                // Aligns tabs to the left
+                indicatorColor: Colors.black26,
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.black26,
+                tabs: busNames.map((name) => Tab(text: name)).toList(),
+              )),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.all(16),
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.only(bottom: 12),
+                  child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    TicketDetailsScreen(
+                                      ticket: TicketDetails(
+                                        busName: busNames[_tabController.index],
+                                        busType: "AC",
+                                        busNumber: "${busNames[_tabController.index].substring(0,2)}-2034",
+                                        departureTime: "10:30 PM",
+                                        arrivalTime: "6:00 AM",
+                                        duration: "7h 30m",
+                                        departureDate: "25 Jan 2025",
+                                        boardingPoint: "Gabtoli Counter",
+                                        droppingPoint: "Chittagong Counter",
+                                        ticketPrice: "৳-850"
+                                      ),
+                                    )
+                            )
+                        );
+                      },
+                      child: TicketCard(
+                        busName: busNames[_tabController.index],
+                      )),
+                );
+              },
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
---
+++ lib/feature/details/presentation/view/ticket_details_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:ticket_system/feature/dashboard/presentation/view/dashboard_screen.dart';
+
+import '../../data/model/ticket_details.dart';
+import '../widget/ticket_details_widget.dart';
+
+class TicketDetailsScreen extends StatelessWidget {
+  final TicketDetails ticket;
+
+  const TicketDetailsScreen({super.key, required this.ticket});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              /// HEADER
+              header(context, ticket.busName),
+              /// BUS IMAGE
+              busImage(),
+              /// BUS INFO
+              infoCard(
+                title: "Bus / Operator Info",
+                children: [
+                  infoRow("Bus Name", ticket.busName),
+                  infoRow("Bus Type", ticket.busType),
+                  infoRow("Bus Number", ticket.busNumber),
+                ],
+              ),
+              /// JOURNEY DETAILS
+              infoCard(
+                title: "Journey Details",
+                children: [
+                  infoRow("Departure Time", ticket.departureTime),
+                  infoRow("Arrival Time", ticket.arrivalTime),
+                  infoRow("Duration", ticket.duration),
+                  infoRow("Boarding Point", ticket.boardingPoint),
+                  infoRow("Dropping Point", ticket.droppingPoint),
+                  infoRow("Ticket Price", ticket.ticketPrice),
+                ],
+              ),
+              /// MAP
+              mapSection(),
+              const SizedBox(height: 10),
+              /// Button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context)=> DashboardScreen()));
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFffe696e),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text("Buy Ticket",style: TextStyle(fontSize: 20,color: Colors.white),),
+                          Icon(Icons.arrow_circle_right_outlined,color: Colors.white,)
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 40),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
